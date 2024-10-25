dd6699aa26a642037d5bbc3085b9238a3cdf6c9c
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-10-25 11:21:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+//your JS code here. If required.
+function displayFormData() {
+	var Fname=document.getElementById("Fname").value;
+	var lname=document.getElementById("lname").value;
+	var pname=document.getElementById("pname").value;
+	var email=document.getElementById("email").value;
+	var message= "First Name:"+Fname+"Last Name:"+lname+"Phone Number:"+pname+"Email ID"+email;
+	alert(message);
+	
+}
